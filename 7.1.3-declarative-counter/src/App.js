e53981082ba0e8b85a76237f9a55cf248845e753
Brandon <rbrandon087@gmail.com>
2023-05-10 21:22:11 -0700

    done

---
+++ 7.1.3-declarative-counter/src/App.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let [ counter, setCounter ] = useState(5);
+  window.setCounter = setCounter;
+
+  return (
+    <div className="App">
+    <h1>{ counter }</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
